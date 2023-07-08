8f47b9a87133ddaa9d88cef7c1eaf2841b81a155
haemalgeumi <haemalgeumi2@gmail.com>
2023-07-08 23:50:28 +0900

    feat: 메인페이지 선택 재료 버튼 기능 추가
    재료 아이템 및 재료 리스트로 컴포넌트화 및 선택 재료 버튼 클릭 시 기능 추가

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,24 +1,19 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useMutation, useQueryClient } from 'react-query';
-// import axios from 'axios';
-import SearchModal from "../components/SearchModal";
 import MainLogo from "../components/MainLogo";
+import IngreList from "../components/IngreList";
+import BasketModal from "../components/BasketModal";
 import styled from "styled-components";
 
-import { FaPlus } from "react-icons/fa";
-import { FaXmark } from "react-icons/fa6";
-
 const MainPage = () => {
-  const [isOpenAddIngre, setIsOpenAddIngre] = useState(false);
-  const [isDeleteBtn, setIsDeleteBtn] = useState(true);
-  const handleAddClick = () => {
-    setIsOpenAddIngre(!isOpenAddIngre);
-  };
-  const handleDeleteClick = () => {
-    setIsDeleteBtn(false);
-  };
   const navigate = useNavigate();
+  const [isOpenBasketModal, setIsOpenBasketModal] = useState(false);
+  const handleBasketClick = () => {
+    setIsOpenBasketModal(!isOpenBasketModal);
+  };
+  const handleCloseBasketModal = () => {
+    setIsOpenBasketModal(false);
+  };
 
   return (
     <StyledWrapper>
@@ -27,25 +22,13 @@ const MainPage = () => {
           <MainLogo />
         </Header>
         <Body>
-          {/* 컴포넌트화 */}
-          <GridContainer>
-            {isDeleteBtn && (
-              <IngreBtn>
-                <Ingre>감자</Ingre>
-                <DeleteBtn onClick={handleDeleteClick}>
-                  <FaXmark />
-                </DeleteBtn>
-              </IngreBtn>
-            )}
-
-            <PlusBtn onClick={handleAddClick}>
-              <FaPlus size="30" />
-            </PlusBtn>
-            {isOpenAddIngre && <SearchModal />}
-          </GridContainer>
+          <IngreList />
           <BottomBtn>
             <Btn onClick={() => navigate("/recipes")}>추천 레시피</Btn>
-            <Btn> 선택 재료</Btn>
+            <Btn onClick={handleBasketClick}> 선택 재료</Btn>
+            {isOpenBasketModal && (
+              <BasketModal onClose={handleCloseBasketModal} />
+            )}
           </BottomBtn>
         </Body>
       </AppBox>
@@ -54,6 +37,7 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
 const BottomBtn = styled.div`
   display: flex;
   justify-content: center;
@@ -99,52 +83,4 @@ const Body = styled.div`
   width: 100%;
   height: 80%;
   padding: 30px;
-`;
-const PlusBtn = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(238, 238, 238, 1);
-  border-radius: 18%;
-  width: 80px;
-  height: 85px;
-  border: none;
-`;
-const IngreBtn = styled.div`
-  //상속하는 거 알아오기
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-
-  background-color: rgba(238, 238, 238, 1);
-  border-radius: 18%;
-  width: 80px;
-  height: 85px;
-`;
-const Ingre = styled.div`
-  font-weight: 600;
-  font-size: 20px;
-  margin-top: 25px;
-`;
-
-const DeleteBtn = styled.button`
-  // 자식 버튼으로 만들기
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 10px;
-  background-color: rgba(198, 197, 197, 1);
-  border-radius: 0;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  width: 80px;
-  height: 29px;
-  border: none;
-`;
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-  grid-gap: 10px;
 `;
