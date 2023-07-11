73ba77f4dbe0a1178ae37248220d60ec098f9105
ShinyoungPark99 <shinyoung011399@gmail.com>
2023-07-12 01:46:13 +0900

    style: 폴더 경로 수정

--- client/src/components/GuestLoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import { GuestButton } from '../styles/SocailLoginStyles';
-import { useNavigate } from 'react-router-dom';
-
-function GuestLoginButton() {
-  const navigate = useNavigate();
-  return <GuestButton onClick={() => navigate('/')}>Guest Login</GuestButton>;
-}
-
-export default GuestLoginButton;
