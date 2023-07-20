1a7dd74252dac013039ca4ef5b666d340890e0b3
haemalgeumi <haemalgeumi2@gmail.com>
2023-07-20 16:10:35 +0900

    feat: 라우트 연결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import WritePage from "./pages/WritePage";
+import MyPage from "./pages/MyPage";
+import MyWritePage from "./pages/MyWritePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<></>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/create-recipe" element={<WritePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/my-writepage" element={<MyWritePage />} />
       </Routes>
     </>
   );
