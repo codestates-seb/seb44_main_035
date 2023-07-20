e32783b54c4a7fa6c2869bd64f08f03267ae4695
ShinyoungPark99 <shinyoung011399@gmail.com>
2023-07-20 22:06:36 +0900

    페이지 추가

---
+++ client/src/pages/BaseketPage.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useRecoilState } from "recoil";
+import { ingreItemAtom } from "../atoms/atoms";
+
+export default function BaseketPage() {
+  // 장바구니에 재료가 있는 경우 => 장바구니 속 재료 모두 있는 레시피 뿌려주기
+  // 선택된 재료 = 장바구니에 담긴 재료 = ingreState (get) -> 보내주신 대로 가져오기
+
+  const [ingreState, setIngreState] = useRecoilState(ingreItemAtom);
+
+  const searchBasket = async () => {
+    try {
+      const headers = {
+        "ngrok-skip-browser-warning": "true",
+      };
+      const queryString = ingreState
+        .map((ingre) => `ingredients=${encodeURIComponent(ingre)}`)
+        .join("&");
+      const url = `${
+        import.meta.env.VITE_API_URL
+      }/recipes/select?${queryString}`;
+
+      const response = await axios.get(url, { headers });
+
+      // 서버로부터 받은 데이터를 처리합니다.
+      console.log(response.data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  return <div>BaseketPage</div>;
+}
---
+++ client/src/pages/RefridgePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function RefridgePage() {
+  //장바구니에 재료 없는 경우, 냉장고 속 재료 하나라도 있으면 뿌려주기
+  //냉장고에 담긴 재료 = ingreList (post) -> 유저 아이디랑 같이 가져오기
+
+  return <div>RefridgePage</div>;
+}
