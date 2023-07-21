1735c134abb2ff00ce39aa8e910a8b1c78be90d4
ShinyoungPark99 <shinyoung011399@gmail.com>
2023-07-22 04:24:05 +0900

    feat: 타입 추가

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -11,3 +11,38 @@ export interface Recipes {
   cookStepContent: string[];
   cookStepImage: string[];
 }
+
+export interface Comments {
+  recipeId: number;
+  userId: number;
+  commentId: number;
+  commentContent: string;
+  createdAt: string;
+  modifiedAt: string;
+}
+
+export interface CommentsPost {
+  commentContent: string;
+}
+
+export interface CommentsEdit {
+  commentId: number;
+  commentContent: string;
+}
+
+export interface Ingredients {
+  ingredientId: number;
+  ingredientName: string;
+  quantity: string;
+}
+
+export interface RecipeDetail {
+  recipeId: number;
+  recipeName: string;
+  recipeImage: string;
+  recipeIntro: string;
+  cookStepContent: [];
+  cookStepImage: string;
+  comments: Comments[];
+  ingredients: Ingredients[];
+}
