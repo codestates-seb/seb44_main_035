f51576d93d29a40a229768af098f0459d9e2fb1a
haemalgeumi <haemalgeumi2@gmail.com>
2023-07-22 23:33:02 +0900

    refactor: type 부분 추가

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -1,13 +1,21 @@
-export interface Ingredient {
+export interface IngredientType {
   ingredientName: string;
   quantity: string;
 }
 
 export interface Recipes {
   recipeName: string;
+  recipeImage: File;
+  recipeIntro: string;
+  ingredients: IngredientType[];
+  cookStepContent: string[];
+  cookStepImage: File[];
+}
+export interface updateRecipe {
+  recipeName: string;
   recipeImage: string;
   recipeIntro: string;
-  ingredients: Ingredient[];
+  ingredients: IngredientType[];
   cookStepContent: string[];
   cookStepImage: string[];
 }
